282d22911479b37a8fc60ad00068dabb4e380235
Robin Williams <56467060+robinawilliams@users.noreply.github.com>
2023-10-10 07:26:24 -0500

    fix for food placement overlap

--- main.py
+++ main.py
@@ -27,15 +27,17 @@ class Snake:
 
 
 class Food:
-
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+        while True:
+            x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+            y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+            # Check if the new food coordinates overlap with the snake
+            if (x, y) not in snake.coordinates:
+                break
 
         self.coordinates = [x, y]
-
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
-
 
 def next_turn(snake, food):
     x, y = snake.coordinates[0]
